75a988e63969a8e5d4ddedb33bd63157c35d03ce
Alexandru Patriciu <apatriciu@gmail.com>
2014-02-26 12:07:49 -0500

    switched to nonpermanent queues and exchanges on the server side

--- computeOpenCL/nova/nova/OpenCL/queues.py
+++ computeOpenCL/nova/nova/OpenCL/queues.py
@@ -1,20 +1,20 @@
 from kombu import Exchange, Queue
 
-task_exchange = Exchange("opencl", type="topic", delivery_mode = 1, 
-                         auto_delete = True, durable = False)
-queue_opencl_devices = Queue("opencl.devices", task_exchange, routing_key = 'opencl.devices',
-                             durable = False, auto_delete = True)
-queue_opencl_contexts = Queue("opencl.contexts", task_exchange, routing_key = 'opencl.contexts',
-                             durable = False, auto_delete = True)
-queue_opencl_buffers = Queue("opencl.buffers", task_exchange, routing_key = 'opencl.buffers',
-                             durable = False, auto_delete = True)
-queue_opencl_programs = Queue("opencl.programs", task_exchange, routing_key = 'opencl.programs', 
-                             durable = False, auto_delete = True)
-queue_opencl_kernels = Queue("opencl.kernels", task_exchange, routing_key = 'opencl.kernels',
-                             durable = False, auto_delete = True)
+task_exchange = Exchange("opencl", type="topic", delivery_mode = 1)#, 
+                         # auto_delete = True, durable = False)
+queue_opencl_devices = Queue("opencl.devices", task_exchange, routing_key = 'opencl.devices')#,
+                             # durable = False, auto_delete = True)
+queue_opencl_contexts = Queue("opencl.contexts", task_exchange, routing_key = 'opencl.contexts')#,
+                             # durable = False, auto_delete = True)
+queue_opencl_buffers = Queue("opencl.buffers", task_exchange, routing_key = 'opencl.buffers')#,
+                             # durable = False, auto_delete = True)
+queue_opencl_programs = Queue("opencl.programs", task_exchange, routing_key = 'opencl.programs')#, 
+                             # durable = False, auto_delete = True)
+queue_opencl_kernels = Queue("opencl.kernels", task_exchange, routing_key = 'opencl.kernels')#,
+                             # durable = False, auto_delete = True)
 queue_opencl_command_queues = Queue("opencl.commandqueues", task_exchange, 
-                             routing_key = 'opencl.commandqueues',
-                             durable = False, auto_delete = True)
-queue_opencl_notify = Queue("openclnotify", task_exchange, routing_key = 'opencl.#',
-                             durable = False, auto_delete = True)
+                             routing_key = 'opencl.commandqueues')#,
+                             # durable = False, auto_delete = True)
+queue_opencl_notify = Queue("openclnotify", task_exchange, routing_key = 'opencl.#')#,
+                             # durable = False, auto_delete = True)
 
